e67df6288d393f4f7f59102b15a2a3ed593df5cb
Daniel Pelsmaeker <info@virtlink.com>
2016-10-11 23:47:26 +0200

    Create initial projects

---
+++ src/Slang/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Slang
+{
+    /// <summary>
+    /// The entry point class.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The entry point method.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        private static void Main(string[] args)
+        {
+            #region Contract
+            if (args == null)
+                throw new ArgumentNullException(nameof(args));
+            #endregion
+
+            throw new NotImplementedException();
+        }
+    }
+}
